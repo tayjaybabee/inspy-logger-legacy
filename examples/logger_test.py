5cac5627f23b35c04cea6298732e0d588096415e
taylor <tayjaybabee@gmail.com>
2020-06-10 00:47:30 -0500

    Remove unneeded file from examples

--- examples/logger_test.py
+++
@@ -1,7 +0,0 @@
-import inspy_logger
-
-log_device = inspy_logger.Logger()
-log = log_device.start('TestApplication', verbose=True)
-
-second_logger = inspy_logger.Logger()
-log_2 = second_logger.start('AnotherApplication')
